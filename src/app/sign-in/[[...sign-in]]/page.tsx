d0e0b538244b4405167c5d28cc9c5f67a881c432
Andrea Petralia <andrea@businesshackscollisionexperts.com>
2026-03-18 15:00:11 -0500

    Remove wrapper so Clerk controls sign-in UI

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -1,20 +1,5 @@
 import { SignIn } from "@clerk/nextjs";
 
 export default function SignInPage() {
-  return (
-    <div
-      style={{
-        minHeight: "100vh",
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-        padding: "24px",
-        background: "rgba(243, 244, 246, 0.75)",
-      }}
-    >
-      <div style={{ width: "100%", maxWidth: 720, minWidth: 360 }}>
-        <SignIn />
-      </div>
-    </div>
-  );
+  return <SignIn />;
 }
