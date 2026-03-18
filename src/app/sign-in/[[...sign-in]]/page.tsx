38e237c551a9652f44bc8ee1f022150720323ba1
Andrea Petralia <andrea@businesshackscollisionexperts.com>
2026-03-18 08:30:00 -0500

    Remove custom styling from sign-in page

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -1,24 +1,5 @@
 import { SignIn } from "@clerk/nextjs";
 
 export default function SignInPage() {
-  return (
-    <div className="min-h-screen bg-gradient-to-br from-slate-900 via-slate-800 to-slate-900 p-4">
-      <div
-        style={{
-          position: 'absolute',
-          top: '50%',
-          left: '50%',
-          transform: 'translate(-50%, -50%)'
-        }}
-      >
-        <SignIn
-          appearance={{
-            variables: {
-              fontSize: '18px'
-            }
-          }}
-        />
-      </div>
-    </div>
-  );
+  return <SignIn />;
 }
